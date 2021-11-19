fd1e06dbd138c3d8c67d5f0b169c20c4f39091df
Or <or.dicker@gmail.com>
2021-11-19 11:49:05 +0200

    running code

--- src/main.cpp
+++ src/main.cpp
@@ -17,12 +17,6 @@ void setup()
   // PWM setup
   pinMode(P_PWM, OUTPUT); // OCR1 output
   
-  //TCCR1A = _BV(COM0B0)| _BV(COM0B1) |_BV(WGM10) | _BV(WGM11) ;
-  //TCCR1B = _BV(CS12) |  _BV(WGM12) | _BV(WGM13);
-  //// 
-  //OCR1A = 125;
-  //OCR1B = 12;
-
   //switch setup
   pinMode(P_SWITCH, INPUT_PULLUP); //switch pin
   
@@ -33,19 +27,11 @@ void setup()
 void loop()
 {
     if (digitalRead(P_SWITCH) == LOW){
-	analogWrite(P_PWM, 229);// 90% pwm
+	analogWrite(P_PWM, 242);// 95% pwm
 	digitalWrite(LED_BUILTIN1, LOW);
     } else {
 	analogWrite(P_PWM, 127);// 50% pwm
 	digitalWrite(LED_BUILTIN1, HIGH);
     }
-    delay(10);
-    //OCR1B = 12; //pwm 10%
-    //// wait for a second
-    //delay(1000);
-    // 
-    //OCR1B = 108; //pwm 90%
-    //// wait for a second
-    //delay(1000);
-    
+    delay(10);    
 }
